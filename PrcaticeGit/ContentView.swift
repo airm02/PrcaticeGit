922a8485dee23bc28f0485ed8c51265bbe0e1e34
Arim <dkflarnjs11@gmail.com>
2023-05-10 11:00:37 +0900

    ㅜㄷㅈ

--- PrcaticeGit/ContentView.swift
+++ PrcaticeGit/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("hello")
+            Text("hello")
+            Text("hello")
         }
         .padding()
     }

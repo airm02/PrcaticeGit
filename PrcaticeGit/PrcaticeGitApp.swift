7ef974d4c66fa85bdc4b9cef7b0bf2e71d2fbb25
Arim <dkflarnjs11@gmail.com>
2023-05-10 10:24:04 +0900

    Initial Commit

---
+++ PrcaticeGit/PrcaticeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrcaticeGitApp.swift
+//  PrcaticeGit
+//
+//  Created by 권아림 on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct PrcaticeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
